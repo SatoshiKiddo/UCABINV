7f0b446845e63fdcd01204859800a1fd7be452ad
Kiddo <41273310+SatoshiKiddo@users.noreply.github.com>
2019-08-26 09:16:55 -0400

    Update app_route.js

--- routes/app_route.js
+++ routes/app_route.js
@@ -6,10 +6,6 @@ router.get("/", function (req,res){
   res.sendFile(path.join(__dirname +'\/..\/views\/app.html'));
 });
 
-router.all("/*[^\wlogout]", function(req, res){
-  res.redirect("/");
-});
-
 router.get("/logout", function(req, res){
   req.session.destroy();
   console.log(req.session);
